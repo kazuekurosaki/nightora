7126e82251453a5430ad1c5c90c48a86e65980be
Kazue Kurosaki <137025030+kazuekurosaki@users.noreply.github.com>
2026-05-13 03:32:19 +0700

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,19 @@
+const videoData = [
+    {
+        id: "Terlalu-Sadis-Caramu",
+        title: "NHDTC-181 Terlalu Sadis Caramu!! - Iori Tachibana",
+        code: "NHDTC-181",
+        thumb: "https://imgjav.shop/img/NHDTC-181/poster_cropped.webp",
+        nsfw: true,
+        release: "2026-03-19",
+        views: 12500, // Untuk fitur Populer
+        isFeatured: true, // Untuk Banner Atas
+        category: "JAV",
+        director: "Tailor Shirai",
+        studio: "Natural High",
+        genres: ["Blowjob", "School Girls", "Slut", "Swallowing"],
+        artists: ["Tachibana Iori"]
+    },
+    
+    // Tambahkan data lainnya mengikuti format di atas
+];
